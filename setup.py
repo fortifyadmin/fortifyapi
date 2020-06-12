f8771b030e0f8c8173599637847ec6d039a63e38
Brandon Spruth <brandon@spruth.co>
2020-06-12 17:22:36 -0500

    removed support for py 2

--- setup.py
+++ setup.py
@@ -48,8 +48,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
