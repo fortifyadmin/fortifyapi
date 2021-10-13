dabd43d4f1da9ac9c2debb073ebdeb90dd0bd522
Brandon Spruth <brandon.spruth@servicenow.com>
2021-10-13 15:28:38 -0500

    fixed diskutils

--- scripts/circleci-pypi-publish.sh
+++ scripts/circleci-pypi-publish.sh
@@ -5,7 +5,7 @@ set -e
 PYPI_CONFIG="${HOME}/.pypirc"
 pip install --upgrade  pip
 pip install twine
-echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
+echo $([distutils]\nindex-servers = pypi\n[pypi]) > $PYPI_CONFIG
 echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
 echo "password=$PYPI_PASSWORD" >> $PYPI_CONFIG
-twine upload dist/*.tar.gz
+twine upload dist/*
