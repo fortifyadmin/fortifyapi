8a78ae64ed15d4c14482dc89857140f8799c7666
Brandon Spruth <brandon.spruth@servicenow.com>
2021-10-13 15:19:04 -0500

    fixed pip upgrade

--- scripts/circleci-pypi-publish.sh
+++ scripts/circleci-pypi-publish.sh
@@ -3,7 +3,7 @@
 set -e
 
 PYPI_CONFIG="${HOME}/.pypirc"
-pip install --upgrade
+pip install --upgrade  pip
 pip install twine
 echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
 echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
