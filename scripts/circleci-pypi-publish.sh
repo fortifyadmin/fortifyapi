4bee02bdadaf9dc7294492d8053c2d328166edbc
Brandon Spruth <brandon.spruth@servicenow.com>
2021-10-13 14:55:28 -0500

    fixed syntax errors with config

--- scripts/circleci-pypi-publish.sh
+++ scripts/circleci-pypi-publish.sh
@@ -3,6 +3,8 @@
 set -e
 
 PYPI_CONFIG="${HOME}/.pypirc"
+pip install --upgrade
+pip install twine
 echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
 echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
 echo "password=$PYPI_PASSWORD" >> $PYPI_CONFIG
